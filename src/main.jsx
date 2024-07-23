5311a188387e5ffe6bb0432b25b0dc69b0101338
Igor Eremin <eremin.igor@icloud.com>
2024-07-23 15:33:30 -0700

    Update routing for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import { NavProvider } from './components/NavContext.jsx'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <NavProvider>
-    <App />
-  </NavProvider>,
+  <Router basename="/my-portfolio">
+    <NavProvider>
+      <App />
+    </NavProvider>
+  </Router>
 )
